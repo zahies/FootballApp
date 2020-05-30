0829ad6ad45bdc583ff82c74803c02a122ef8f18
zahies <kaprizahi@gmail.com>
2020-05-30 14:19:36 +0300

    fixing db connection

--- app/src/test/java/SpringControllers/MemberControllerTest.java
+++ app/src/test/java/SpringControllers/MemberControllerTest.java
@@ -1,10 +1,18 @@
 package SpringControllers;
 
+import DataAccess.Exceptions.DuplicatedPrimaryKeyException;
+import DataAccess.Exceptions.NoConnectionException;
+import DataAccess.Exceptions.mightBeSQLInjectionException;
 import Domain.FootballManagmentSystem;
 import Domain.Users.Commissioner;
 import Domain.Users.Member;
 import Domain.Users.TeamOwner;
+import FootballExceptions.NoPermissionException;
+import FootballExceptions.UserInformationException;
+import FootballExceptions.UserIsNotThisKindOfMemberException;
 import org.junit.Before;
+
+import java.sql.SQLException;
 
 import static org.junit.Assert.*;
 
@@ -18,7 +26,7 @@ public class MemberControllerTest {
 
 
     @Before
-    public void init(){
+    public void init() throws mightBeSQLInjectionException, DuplicatedPrimaryKeyException, NoPermissionException, SQLException, UserInformationException, UserIsNotThisKindOfMemberException, NoConnectionException {
         //teamowner = new TeamOwner("Moshe","DASD",123,"asd");
         com = new Commissioner("zaza",12,"123","zahi zahi");
         memberController = new MemberController();
